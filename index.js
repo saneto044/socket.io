7dc7e6bc87c11d44408c6e16542e0ef47a7db146
unknown <sa.jrneto@gmail.com>
2022-05-12 15:08:03 -0300

    first

---
+++ index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+io.on("connection",(socket) => {
+    
+    socket.on('disconnect',() => {
+        console.log("X desconectou:" + socket.id )
+    })
+    
+    socket.on("msg",(data) => {
+        io.emit('showmsg',data);
+        console.log("username:" + data.username + " a mensagem " + data.msg)
+    })
+});
+
+
+app.set("view engine", "ejs");
+
+app.get('/' , (req,res) => {
+    res.render('index');
+})
+
+http.listen(3000,() => {
+    console.log("App Rodando!!")
+})
